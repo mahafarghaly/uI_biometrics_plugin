f8030e6f70102b190490d11bfce5fc4c199976c0
Maha Farghaly <mahafarghalymo@gmail.com>
2025-06-01 14:55:23 +0300

    Update to version  0.0.2
    *Move full example into single main.dart file
    *Add changelog for version 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-## 0.0.1
 
-* TODO: Describe initial release.
+# Changelog
+
+## [0.0.2] - 2025-06-01
+- Improved example app by consolidating all code into a single file.
+
+## [0.0.1] - Initial release
+- Initial release of the biometrics_plugin package.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:biometrics_plugin/biometrics_plugin.dart';
 import 'package:ui_biometrics_plugin/biometric_wrapper.dart';
-
-import 'biometric_screen.dart';
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
@@ -33,6 +31,35 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(title: const Text('Biometric UI Plugin'),backgroundColor: Colors.amber,),
         body: BiometricsScreen(),
+      ),
+    );
+  }
+}
+class BiometricsScreen extends StatelessWidget{
+  BiometricsScreen({super.key});
+  final _wrapperKey = GlobalKey<BiometricWrapperState>();
+  BiometricStatus? biometricStatus;
+  @override
+  Widget build(BuildContext context) {
+    return BiometricWrapper(
+      userCredentials: {"name":"maha","password":"1234"},
+      biometricOnly: false,
+      key: _wrapperKey,
+      triggerManually: true,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Authentication Result: UI Biometrics plugin'),
+            ElevatedButton(
+              onPressed: () async {
+                biometricStatus= await _wrapperKey.currentState?.triggerAuth();
+                print("@@@✅$biometricStatus");
+              },
+              child: Text("Authenticate with Biometrics"),
+            ),
+          ],
+        ),
       ),
     );
   }
